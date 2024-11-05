f65fc91e58db232c4d60d3b8cf1bc87fa9ec4f90
Guilherme <guilhermetarter@hotmail.com>
2024-11-05 19:53:57 -0300

    fix tests

--- redes2024_ob2/tests/jsonrpc_test.sh
+++ redes2024_ob2/tests/jsonrpc_test.sh
@@ -1,12 +1,12 @@
 sh echo "=============================================="
 sh echo "Capturando tráfico en client, vhosts y servers"
 sh echo "=============================================="
-client tcpdump -i any -w jsonrpc_test_client.pcap > /dev/null 2>&1 &
-vhost1 tcpdump -i any -w jsonrpc_test_vhost1.pcap > /dev/null 2>&1 &
-vhost2 tcpdump -i any -w jsonrpc_test_vhost2.pcap > /dev/null 2>&1 &
-vhost3 tcpdump -i any -w jsonrpc_test_vhost3.pcap > /dev/null 2>&1 &
-server1 tcpdump -i any -w jsonrpc_test_server1.pcap > /dev/null 2>&1 &
-server2 tcpdump -i any -w jsonrpc_test_server2.pcap > /dev/null 2>&1 &
+client tcpdump -i any -w capturas/jsonrpc_test_client.pcap > /dev/null 2>&1 &
+vhost1 tcpdump -i any -w capturas/jsonrpc_test_vhost1.pcap > /dev/null 2>&1 &
+vhost2 tcpdump -i any -w capturas/jsonrpc_test_vhost2.pcap > /dev/null 2>&1 &
+vhost3 tcpdump -i any -w capturas/jsonrpc_test_vhost3.pcap > /dev/null 2>&1 &
+server1 tcpdump -i any -w ../capturas/jsonrpc_test_server1.pcap > /dev/null 2>&1 &
+server2 tcpdump -i any -w ../capturas/jsonrpc_test_server2.pcap > /dev/null 2>&1 &
 sh sleep 2
 
 sh echo "=========================================="
